71b6baf104225ce43410be19a7914aa3b3e509c1
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2022-04-19 00:43:23 +0900

    feat: Added set up and sample tutorial

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # pytorch_learning
+
+## Set up
+
+I'm gonna assume you're cool and on Linux.
+1. Install the [CUDA Toolkit](https://developer.nvidia.com/cuda-downloads) for great justice.
+2. Set up the Python environment
+   ```shell
+   python -m venv venv
+   source venv/bin/activate
+   python -m pip install --upgrade pip
+   python -m pip install -r requirements.txt
+   ```
+   You can just copy and paste this safely.
+3. To test everything is working, try `python sample_tutorial.py`. This will download a sample dataset, train a model on it, and have the model guess at the contents of an image in the dataset.
---
+++ sample_tutorial.py
@@ -0,0 +1,156 @@
+# mostly from https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+# plus beege's refactoring
+from typing import Tuple
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+
+def prepare_datasets() -> Tuple[Dataset, Dataset]:
+    # Download training data from open datasets.
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    return training_data, test_data
+
+
+def prepare_dataloaders(training_data, test_data) -> Tuple[DataLoader, DataLoader]:
+    batch_size = 64
+
+    # Create data loaders.
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
+    return train_dataloader, test_dataloader
+
+
+def get_device() -> str:
+    # Get cpu or gpu device for training.
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
+    return device
+
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+def test(device, dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+def train(device, dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (x, y) in enumerate(dataloader):
+        x, y = x.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(x)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(x)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def main():
+    training_data, test_data = prepare_datasets()
+    train_dataloader, test_dataloader = prepare_dataloaders(training_data, test_data)
+    device = get_device()
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    # https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+    loss_fn = nn.CrossEntropyLoss()
+    # SGD - stochastic gradient descent
+    # https://optimization.cbe.cornell.edu/index.php?title=Stochastic_gradient_descent
+    # https://towardsdatascience.com/stochastic-gradient-descent-clearly-explained-53d239905d31
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train(device, train_dataloader, model, loss_fn, optimizer)
+        test(device, test_dataloader, model, loss_fn)
+    print("Done!")
+
+    torch.save(model.state_dict(), "model.pth")
+    print("Saved PyTorch Model State to model.pth")
+
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load("model.pth"))
+
+    classes = [
+        "T-shirt/top",
+        "Trouser",
+        "Pullover",
+        "Dress",
+        "Coat",
+        "Sandal",
+        "Shirt",
+        "Sneaker",
+        "Bag",
+        "Ankle boot",
+    ]
+
+    model.eval()
+    x, y = test_data[0][0], test_data[0][1]
+    with torch.no_grad():
+        pred = model(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+
+if __name__ == '__main__':
+    main()
